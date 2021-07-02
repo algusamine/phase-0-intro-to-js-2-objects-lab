9db9c83eabad0b8eaf418cfada5e1c59b5a65498
Amine <algusamine@gmail.com>
2021-07-01 22:52:13 -0400

    commit tree c7e36b45ff13c1384c78a2f03054bcb8057be9e3

--- index.js
+++ index.js
@@ -1,31 +1,25 @@
 // Write your solution in this file!
 const employee = {
-    name: 'Adam',
-    streetAddress: '502 west 141st street'
-};
-//1: 
-function updateEmployeeWithKeyAndValue(employee, key, value){
-    const newEmployee = {...employee};
-    newEmployee[key] = value;
-    return newEmployee;
-};
-//1:
-/*function updateEmployeeWithKeyAndValue(employee, key, value){
-    return Object.assign({}, employee, {[key]: value});
-}; */
-//2:
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-    employee[key] = value;
-    return employee;
-};
-//3:
-function  deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = {...employee};
-    delete newEmployee[key];
-    return newEmployee;
+    name: 'Sam'
 }
-//4:
-function  destructivelyDeleteFromEmployeeByKey(employee, key) {
-    delete employee[key];
-    return employee;
+function updateEmployeeWithKeyAndValue(obj, key, value){
+//return Object.assign({}, obj, {[key]: value});
+const newObj = {...obj};
+newObj[key] = value;
+return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+    obj[key] = value;
+    return obj;
+}
+function deleteFromEmployeeByKey(obj, key){
+    const newObj = {...obj}
+    delete newObj[key];
+    return newObj;
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key){
+    delete obj[key]
+    return obj;
 }
